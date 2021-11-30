6908690ff9c0bc409425bc9b1688898579e60f8b
SAUL NJIE <SAULUS92@gmail.com>
2021-11-30 14:01:21 +0100

    This fucking search fuck

--- js/libs/index.js
+++ js/libs/index.js
@@ -1,5 +1,5 @@
 function liveSearch() {
-    let cards = document.querySelectorAll('.album_card');
+    let cards = document.querySelectorAll('.albums');
     let search_query = document.getElementById('searchbox').value;
     for (var i = 0; i < cards.length; i++) {
         if (cards[i].innerText.toLowerCase().includes(search_query.toLowerCase())) {
