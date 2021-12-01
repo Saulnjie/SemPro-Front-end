a31338abe4aeb607174218467735e94bd9f9b143
SAUL NJIE <SAULUS92@gmail.com>
2021-12-01 16:39:26 +0100

    ds

--- js/detailed.js
+++ js/detailed.js
@@ -1,6 +1,7 @@
 import { renderAlbum } from "./album.js";
 import { getAlbum } from "./data.js";
 
+
 const queryString = document.location.search; 
 const params = new URLSearchParams(queryString); 
 const id = params.get('id'); 
