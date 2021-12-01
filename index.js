e6d1151347f133aa09a734c5996379b099624426
SAUL NJIE <SAULUS92@gmail.com>
2021-12-01 16:27:37 +0100

    ILOVEIT

---
+++ index.js
@@ -0,0 +1,21 @@
+
+function liveSearch() {
+    let cards = document.querySelectorAll('.album_card');
+    let search_query = document.getElementById('searchbox').value;
+    for (var i = 0; i < cards.length; i++) {
+        if (cards[i].innerText.toLowerCase().includes(search_query.toLowerCase())) {
+            cards[i].classList.remove('is-hidden');
+        } else {
+            cards[i].classList.add('is-hidden');
+        }
+    }
+}
+
+let typingTimer;
+let typeInterval = 500;
+let searchInput = document.getElementById('searchbox');
+
+searchInput.addEventListener('keyup', () => {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(liveSearch, typeInterval);
+});
--- js/components/async.js
+++ js/components/async.js
@@ -1,49 +1,7 @@
-export async function getMeData() {
-	const response = await fetch('https://cammiesrecords.herokuapp.com/albums');
-	const data = await response.json();
-	console.log('All results from data', data);
-	return data;
-}
+import {renderAlbum } from "../album.js" 
+import {getAllAlbums } from "../data.js" 
 
-getMeData().then(albums => albums.forEach(renderAlbum));
-
-
-
-export function renderAlbum(album) {
-	const buttonId = `heart-button-${album.id}`
-
-	const albumCard = document.createElement("div")
-
-	albumCard.classList.add("album_card")
-	albumCard.innerHTML = `
-		<h1>${album.ArtistName}</h1>
-		<h2>${album.AlbumName}</h2>
-		<p>${album.Description}</p>
-		<p>${album.Price}</p>
-		<button id="${buttonId}">
-		<i class="far fa-heart" data-name="${album.ArtistName}" data-album="${album.AlbumName}" data-price="${album.Price}"></i>
-		</button>
-	`
-
-	document.querySelector('.albums').appendChild(albumCard)
-
-	const heartButton = document.getElementById(buttonId);
-	heartButton.onclick = () => {
-		
-		let favorites = getStoredFavorites()
-		const isAdded = favorites.some(item => item.id === album.id)
-
-		if (isAdded) {
-			const index = favorites.indexOf(album)
-			favorites.splice(index, 1)
-			favorites = favorites;
-		} else {
-			favorites.push(album)
-		}
-
-		localStorage.setItem("favorites", JSON.stringify(favorites))
-	}
-}
+getAllAlbums().then(albums => albums.forEach(renderAlbum));
 
 
 export function getStoredFavorites() {
