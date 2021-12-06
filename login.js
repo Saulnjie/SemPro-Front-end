b7e7d04c179e4963cf1410c21bb5586565dfe5ae
SAUL NJIE <SAULUS92@gmail.com>
2021-12-06 17:25:28 +0100

    Admin logic

---
+++ login.js
@@ -0,0 +1,34 @@
+const form = document.getElementById("login-form")
+const loggedInContent = document.querySelector("#logged-in-content")
+
+
+form.onsubmit = async (event) => {
+  event.preventDefault()
+
+  const email = document.querySelector("#email").value
+  const password = document.querySelector("#password").value
+
+  const body = {
+    identifier: email,
+    password,
+  }
+
+  const headers = {
+    "Content-Type": "application/json"
+  }
+
+  const response = await fetch("https://cammiesrecords.herokuapp.com/auth/local", {
+      method: "POST",
+      body: JSON.stringify(body),
+      headers,
+  })
+
+  if (!response.ok) {
+     // handle error
+  }
+
+  const { jwt } = await response.json()
+  localStorage.setItem("strapi-access-token", jwt)
+
+  window.location.assign("/admin.html")
+}
