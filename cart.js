fb917b11be02744aa3e8b7b95c52132e18d2d523
SAUL NJIE <SAULUS92@gmail.com>
2021-12-14 13:53:23 +0100

    Styling for Aces

--- cart.js
+++ cart.js
@@ -4,3 +4,10 @@ import { renderAlbum } from "./js/album.js";
 const favorites = getStoredFavorites()
 
 favorites.forEach(album => renderAlbum(album))
+
+
+// else {
+//     document.querySelector('.cart').innerHTML = `
+//     <h2 class="cart_alert">Your cart is empty!</h2>`
+// }
+
--- js/helpers.js
+++ js/helpers.js
@@ -5,7 +5,11 @@ export function getStoredFavorites() {
 	if (storedFavorites) {
 		const parsed = JSON.parse(storedFavorites)
 		favorites = parsed
-	}
+	} 
+	// else {
+	// 	document.querySelector('.cart').innerHTML = `
+	// 	<h2 class="cart_alert">Your cart is empty!</h2>`
+	// }
 
 	return favorites
 }
